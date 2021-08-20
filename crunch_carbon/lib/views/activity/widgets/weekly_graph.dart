51210eaa09d219b52c7ed6ee06034754cdbdeb14
UcGeorge <petegeorge20005@gmail.com>
2021-08-20 23:15:17 +0100

    Weekly graph done

--- crunch_carbon/lib/views/activity/widgets/weekly_graph.dart
+++ crunch_carbon/lib/views/activity/widgets/weekly_graph.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class WeeklyGraph extends StatelessWidget {
   const WeeklyGraph({
@@ -13,18 +14,245 @@ class WeeklyGraph extends StatelessWidget {
         color: Colors.black,
         borderRadius: BorderRadius.circular(15),
       ),
-      child: Column(
+      child: Stack(
         children: [
-          Text(
-            'Total Carbon Footprint',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 21,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 1.0,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Text(
+                  '1230.23',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 29,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Text(
+                  'Weekly Usage',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.65),
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(child: BarChartSample3()),
+            ],
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              height: 42,
+              width: 114,
+              margin: EdgeInsets.only(right: 20),
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                border: Border.all(
+                  color: Colors.white,
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(6),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.share,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                  Text(
+                    'Share',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      // letterSpacing: 1.5,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class BarChartSample3 extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => BarChartSample3State();
+}
+
+class BarChartSample3State extends State<BarChartSample3> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(18),
+        ),
+        // color: Color(0xff232d37),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(right: 20.0, left: 12),
+        child: TheBarChart(),
+      ),
+    );
+  }
+}
+
+class TheBarChart extends StatelessWidget {
+  TheBarChart({
+    Key? key,
+  }) : super(key: key);
+
+  final barRodData = BarChartRodData(
+    y: 250,
+    colors: [const Color(0xff147AD6)],
+    width: 22,
+    borderRadius: BorderRadius.circular(4),
+  );
+
+  final data = [250, 500, 740, 400, 550, 300, 420];
+
+  @override
+  Widget build(BuildContext context) {
+    int index = 0;
+    return BarChart(
+      BarChartData(
+        alignment: BarChartAlignment.spaceBetween,
+        maxY: 1000,
+        barTouchData: BarTouchData(
+          touchTooltipData: BarTouchTooltipData(
+            tooltipBgColor: Colors.white,
+            getTooltipItem: (
+              BarChartGroupData group,
+              int groupIndex,
+              BarChartRodData rod,
+              int rodIndex,
+            ) {
+              return BarTooltipItem(
+                rod.y.toString().substring(0, rod.y.toString().indexOf('.')) +
+                    ' Carbon Used',
+                TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  // letterSpacing: 1.0,
+                ),
+              );
+            },
+          ),
+        ),
+        gridData: FlGridData(
+          show: true,
+          drawHorizontalLine: true,
+          horizontalInterval: 20,
+          // checkToShowHorizontalLine: (value) => value == 480,
+          getDrawingHorizontalLine: (value) {
+            if (value == 480) {
+              return FlLine(
+                color: const Color(0xff7C828A),
+                strokeWidth: 1,
+                dashArray: [1, 3],
+              );
+            }
+            return FlLine(
+              color: Colors.transparent,
+              strokeWidth: 0.8,
+            );
+          },
+        ),
+        titlesData: FlTitlesData(
+          show: true,
+          leftTitles: SideTitles(
+            showTitles: true,
+            getTextStyles: (context, value) => const TextStyle(
+              color: Color(0xff67727d),
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
+            interval: 50,
+            getTitles: (value) {
+              switch (value.toInt()) {
+                // case 0:
+                //   return '0';
+                case 250:
+                  return '250';
+                // case 200:
+                //   return '200';
+                case 500:
+                  return '500';
+                // case 400:
+                //   return '400';
+                case 750:
+                  return '750';
+              }
+              return '';
+            },
+            reservedSize: 28,
+            margin: 20,
+          ),
+          bottomTitles: SideTitles(
+            showTitles: true,
+            getTextStyles: (context, value) => const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 14),
+            margin: 20,
+            getTitles: (double value) {
+              switch (value.toInt()) {
+                case 0:
+                  return 'M';
+                case 1:
+                  return 'T';
+                case 2:
+                  return 'W';
+                case 3:
+                  return 'T';
+                case 4:
+                  return 'F';
+                case 5:
+                  return 'S';
+                case 6:
+                  return 'S';
+                default:
+                  return '';
+              }
+            },
+          ),
+        ),
+        borderData: FlBorderData(
+          show: false,
+        ),
+        barGroups: data
+            .map((e) => BarChartGroupData(
+                  x: index++,
+                  barRods: [
+                    barRodData.copyWith(
+                      y: e.toDouble(),
+                      colors: [
+                        e <= 480
+                            ? Color(0xff7388A9).withOpacity(0.35)
+                            : const Color(0xff147AD6)
+                      ],
+                    )
+                  ],
+                  // showingTooltipIndicators: [0],
+                ))
+            .toList(),
       ),
     );
   }
